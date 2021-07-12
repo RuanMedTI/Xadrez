c301441960293a3399a6c19f1523c2d4d7100c10
RuanMedTI <rurato2002@gmail.com>
2021-07-12 10:56:21 -0300

    comecando implementar o tabuleiro

--- Xadrez.ConsoleApp/Program.cs
+++ Xadrez.ConsoleApp/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using tabuleiro;
 
 namespace Xadrez.ConsoleApp
 {
@@ -10,6 +7,8 @@ namespace Xadrez.ConsoleApp
     {
         static void Main(string[] args)
         {
+            Tabuleiro tab = new Tabuleiro(8, 8);
+            Console.ReadLine();
         }
     }
 }
